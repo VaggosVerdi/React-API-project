f58585402bfa675946cd56f88b17921048912222
VaggosVerdi <56359698+VaggosVerdi@users.noreply.github.com>
2025-04-13 18:09:53 +0300

    Update App.jsx

--- React-API-project/src/App.jsx
+++ React-API-project/src/App.jsx
@@ -6,7 +6,7 @@ import './App.sass'
 export default function App() {
     return (
         <>
-            <h1 className="text-center mb-3">Cobalt Fairy - Trial</h1>
+            <h1 className="text-center mb-3">React API Project</h1>
             <div className="menu sticky-top mb-5">
                 <a href="#table" className="nav-link mx-4">Data Table</a>
                 <a href="#form" className="nav-link mx-4">Form</a>
